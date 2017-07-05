cab558ede44bcab1f8c75f9c478e8da69d0cacd0
Hugh Dickinson <hdickins@umn.edu>
2017-07-05 09:49:10 +0100

    Bug fixes

--- tools/database/load-panoptes-csv.py
+++ tools/database/load-panoptes-csv.py
@@ -26,18 +26,18 @@ def main() :
     db = swap.db.DB()
 
     argParser = argparse.ArgumentParser(prefix_chars='-')
-    argParser.add_argument('panoptes-dumpfile')
+    argParser.add_argument('panoptesdumpfile')
     argParser.add_argument('--limit-records', nargs='?', const=1, type=int, default=-1)
     argParser.add_argument('--dryrun', nargs='?', const=True, type=bool, default=False)
     args = argParser.parse_args()
-    print("Using file %s" % args.panoptes_dumpfile)
+    print("Using file %s" % args.panoptesdumpfile)
 
-    if not os.path.isfile(args.file):
-        raise FileNotFoundError("Couldn't find file at '%s'" % args.panoptes_dumpfile)
+    if not os.path.isfile(args.panoptesdumpfile):
+        raise FileNotFoundError("Couldn't find file at '%s'" % args.panoptesdumpfile)
     if args.file.split('.')[-1] != 'csv':
-        raise ValueError("File '%s' not a valid csv file" % args.panoptes_dumpfile)
+        raise ValueError("File '%s' not a valid csv file" % args.panoptesdumpfile)
 
-    csvParser = CsvDumpParser(panoptes_dumpfile)
+    csvParser = CsvDumpParser(args.panoptesdumpfile)
     flattenedData = csvParser.getUnpackedData(skipUpackingFor = config.database.panoptes_builder.skip_unpack_columns, rowRange = (0, args.limit_records))
 
     dataForUpload = []
