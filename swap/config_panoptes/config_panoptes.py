f5aef66f6555e364c3a2a852ce4430de61148c0a
Hugh Dickinson <hdickins@umn.edu>
2017-07-06 15:16:50 +0100

    Added missing namespace.

--- swap/config_panoptes/config_panoptes.py
+++ swap/config_panoptes/config_panoptes.py
@@ -28,7 +28,7 @@ class database:
                 'subject_id': {'type': int},
             }
 
-            db_to_panoptes_csv_map = { key : { 'panoptes_key' : None, 'converter_func' : lambda x : x if (isinstance(x, Number) and not np.isnan(x)) else None } for key in db_schema.keys() }
+            db_to_panoptes_csv_map = { key : { 'panoptes_key' : None, 'converter_func' : lambda x : x if (isinstance(x, numbers.Number) and not np.isnan(x)) else None } for key in db_schema.keys() }
 
             db_to_panoptes_csv_map['classification_id']['panoptes_key'] = 'classification_id'
             db_to_panoptes_csv_map['user_id']['panoptes_key'] = 'user_id'
