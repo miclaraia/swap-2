ae9b0cd288fd473195eb76315cc5d71504c99189
Michael Laraia <micheal.laraia@gmail.com>
2017-07-21 14:54:24 +0100

    Modified how url's are generated

--- swap/caesar/utils/address.py
+++ swap/caesar/utils/address.py
@@ -1,19 +1,43 @@
 
-import swap.config as config
+import swap.config
 from swap.caesar.auth import Auth
 
 import logging
+from functools import wraps
 
 logger = logging.getLogger(__name__)
 
 
+def swap_wrapper(func):
+    def wrapper(cls, auth=True):
+        protocol = cls.config().address._swap_protocol
+
+        route = func(cls)
+        root = cls._swap_root()
+
+        if auth:
+            auth_string = Auth().http_string()
+            root = protocol + auth_string + root
+        else:
+            root = protocol + root
+
+        address = '%s/%s' % (root, route)
+        return address
+    return wrapper
+
+
 class Address:
-    config = config.online_swap
+
+    @staticmethod
+    def config():
+        return swap.config.online_swap
 
     @classmethod
     def panoptes(cls):
-        endpoint = cls.config.caesar.panoptes_endpoint
-        url = cls.config.address._panoptes
+        config = cls.config()
+
+        endpoint = config.caesar.panoptes_endpoint
+        url = config.address._panoptes
 
         return url % {'endpoint': endpoint}
 
@@ -23,30 +47,40 @@ class Address:
         Returns the root workflow address for caesar
         eg: https://caesar.zooniverse.org:443/workflows/1234
         """
-        url = cls.config.address._caesar
+        config = cls.config()
 
-        endpoint = cls.config.caesar.caesar_endpoint
-        port = cls.config.caesar.port
-        workflow = cls.config.workflow
+        url = config.address._caesar
+
+        endpoint = config.caesar.caesar_endpoint
+        port = config.caesar.port
+        workflow = config.workflow
         return url % {'endpoint': endpoint, 'port': port, 'workflow': workflow}
 
     @classmethod
     def reducer(cls):
-        reducer = cls.config.caesar.reducer
-        addr = cls.config.address._reducer
+        config = cls.config()
+
+        reducer = config.caesar.reducer
+        addr = config.address._reducer
         return cls.root() + addr % {'reducer': reducer}
 
     @classmethod
-    def swap_classify(cls):
-        addr = cls.config.address._swap
-        host = cls.config.host
-        port = cls.config.ext_port
-        route = cls.config.route
+    def _swap_root(cls):
+        config = cls.config()
 
-        auth = Auth()
-        username = auth._username
-        password = auth._key
+        addr = config.address._swap
+        host = config.host
+        port = config.ext_port
+        project = config.project
 
-        return addr % \
-            {'user': username, 'pass': password,
-             'host': host, 'port': port, 'route' : route}
+        return addr % {'host': host, 'port': port, 'project': project}
+
+    @classmethod
+    @swap_wrapper
+    def swap_classify(cls, auth=True):
+        return 'classify'
+
+    @classmethod
+    @swap_wrapper
+    def swap_scores(cls, auth=True):
+        return 'scores'
