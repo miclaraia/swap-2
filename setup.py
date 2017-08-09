e3f9c5e68f61a190e67ac599ec6b2ea6fa3bc200
Michael Laraia <micheal.laraia@gmail.com>
2017-08-09 14:36:20 -0500

    Keep version in static version file
    Keep the semantic version in a static version file,
    and have python setuptools source the version number
    from this file

--- setup.py
+++ setup.py
@@ -13,6 +13,10 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
+# Get the version string from the VERSION file
+with open(path.join(here, 'VERSION', 'r')) as f:
+    version = f.readline()
+
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
@@ -23,7 +27,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.2',
+    version=version,
 
     description='SWAP algorithm for a Zooniverse project',
     long_description=long_description,
