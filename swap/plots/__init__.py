88289c217e6d49e152d3ee4e246605b5036e173d
Michael Laraia <micheal.laraia@gmail.com>
2017-09-01 15:42:28 -0500

    Added Plots module successful import marker
    Plots module marks whether plotting libraries were import successfully.

--- swap/plots/__init__.py
+++ swap/plots/__init__.py
@@ -10,12 +10,9 @@ try:
     from swap.plots.performance import plot_confusion_matrix
     # from swap.plots.performance import plot_histogram
     from swap.plots.performance import plot_roc
+
+    _active = True
 except ImportError:
-    traces = None
-    distributions = None
-    performance = None
-    plot_pdf = None
-    plot_class_histogram = None
-    plot_user_cm = None
-    plot_confusion_matrix = None
-    plot_roc = None
+    _active = False
+    import warnings
+    warnings.warn('Plotting is offline. Can\'t import necessary modules')
