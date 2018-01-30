5e40ae0fff9ce52c0c98a471b5a2aa60eb386a07
Michael Laraia <micheal.laraia@gmail.com>
2018-01-30 16:39:42 -0600

    Added click dependency

--- setup.py
+++ setup.py
@@ -83,6 +83,7 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
         'h5py',
+        'click',
     ],
 
     # List additional groups of dependencies here (e.g. development
